24fda953e7ffc8dde97e72ae8cdca962af0978d5
William Rodrigues <williamrodrigues2442@gmail.com>
2024-04-02 13:56:57 -0300

    chore: add env to database path

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 
-const storagePath = process.env.BOT_ENV === 'dev' ? 'db.dev.sqlite3' : 'db.prod.sqlite3';
-
 module.exports = new Sequelize({
   dialect: 'sqlite',
   logging: process.env.BOT_ENV === 'dev' ? console.log : false,
-  storage: storagePath,
+  storage: process.env.DATABASE_PATH,
 });
